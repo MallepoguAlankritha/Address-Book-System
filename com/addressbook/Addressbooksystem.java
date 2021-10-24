429fc52251c378ec54ea755980d8759408f72c7e
MallepoguAlankritha <mallepogualankritha@gmail.com>
2021-10-24 19:18:37 +0530

    Added:Welcome to Address Book System

---
+++ com/addressbook/Addressbooksystem.java
@@ -0,0 +1,14 @@
+package com.addressbook;
+
+public class Addressbooksystem {
+	static void welcome(){
+        System.out.println("Welcome to Address Book System Program");
+    }
+
+
+	public static void main(String[] args) {
+		 welcome();
+
+	}
+
+}
